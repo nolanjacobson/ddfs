87b2163324485bc708defe71f3950b2bb82c1e25
EvanErickson <evanserickson@gmail.com>
2020-05-23 17:58:24 -0700

    added signin
    Co-authored-by: Nolan Jacobson <50815660+nolanjacobson@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,15 @@ import Page from './pages/Page'
 import Page2 from './pages/Page2'
 import HomePage from './pages/HomePage'
 import NotFound from './pages/NotFound'
+import SignIn from './components/signIn'
 
 const App = () => {
   return (
     <Router>
-      <header>
-        <h1>Welcome to my SPA</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Go to LA </Link>
-            </li>
-            <li>
-              <Link to="/1">Page 1</Link>
-            </li>
-            <li>
-              <Link to="/2">Page 2</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/1" component={Page}></Route>
-        <Route exact path="/2" component={Page2}></Route>
+        <Route exact path="/login" component={SignIn}></Route>
         <Route path="*" component={NotFound}></Route>
       </Switch>
     </Router>

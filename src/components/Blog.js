b59ff6091006244e4b0c043323d91ccdc4d1fbe2
EvanErickson <evanserickson@gmail.com>
2020-05-23 18:22:39 -0700

    changes

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,9 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-// import GitHubIcon from '@material-ui/icons/GitHub';
-// import FacebookIcon from '@material-ui/icons/Facebook';
-// import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
 import Header from './Header';
 import MainFeaturedPost from './MainFeaturedPost';
 import FeaturedPost from './FeaturedPost';
@@ -83,9 +83,9 @@ const sidebar = {
     { title: 'April 1999', url: '#' },
   ],
   social: [
-    // { name: 'GitHub', icon: GitHubIcon },
-    // { name: 'Twitter', icon: TwitterIcon },
-    // { name: 'Facebook', icon: FacebookIcon },
+    { name: 'GitHub', icon: GitHubIcon },
+    { name: 'Twitter', icon: TwitterIcon },
+    { name: 'Facebook', icon: FacebookIcon },
   ],
 };
 

a02ed9cd12c6ec584a2e84c160a7c9ce34384e4f
EvanErickson <evanserickson@gmail.com>
2020-05-25 12:47:01 -0700

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 import NotFound from './pages/NotFound'
-import SignIn from './components/signIn'
+import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
-import Blog from './components/Blog'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Blog}></Route>
+        <Route exact path="/" component={SignIn}></Route>
         <Route exact path="/signup" component={SignUp}></Route>
         <Route exact path="/signin" component={SignIn}></Route>
         <Route path="*" component={NotFound}></Route>

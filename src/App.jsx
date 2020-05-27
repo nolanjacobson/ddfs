b6a47609722123de4e6aa965bb3d005908b90449
EvanErickson <evanserickson@gmail.com>
2020-05-26 17:27:12 -0700

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 import NotFound from './pages/NotFound'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={SignIn}></Route>
+        <Route exact path="/" component={Home}></Route>
         <Route exact path="/signup" component={SignUp}></Route>
         <Route exact path="/signin" component={SignIn}></Route>
         <Route path="*" component={NotFound}></Route>
--- src/components/signIn.jsx
+++ src/components/signIn.jsx
@@ -18,7 +18,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        DDFS
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -115,7 +115,7 @@ export default function SignInSide() {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/signup" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
